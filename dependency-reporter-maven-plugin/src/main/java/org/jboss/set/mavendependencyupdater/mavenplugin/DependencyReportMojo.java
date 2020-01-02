e97ea297aa57d6c4f6f4badc1ea584c05061f791
Tomas Hofman <thofman@redhat.com>
2020-01-02 18:09:20 +0100

    Create maven plugin

--- dependency-reporter-maven-plugin/src/main/java/org/jboss/set/mavendependencyupdater/mavenplugin/DependencyReportMojo.java
+++ dependency-reporter-maven-plugin/src/main/java/org/jboss/set/mavendependencyupdater/mavenplugin/DependencyReportMojo.java
@@ -8,10 +8,22 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectDependenciesResolver;
+import org.commonjava.maven.ext.common.ManipulationException;
+import org.jboss.set.mavendependencyupdater.AvailableVersionsResolver;
+import org.jboss.set.mavendependencyupdater.DefaultAvailableVersionsResolver;
+import org.jboss.set.mavendependencyupdater.DependencyEvaluator;
+import org.jboss.set.mavendependencyupdater.common.ident.ScopedArtifactRef;
+import org.jboss.set.mavendependencyupdater.configuration.Configuration;
+import org.jboss.set.mavendependencyupdater.core.processingstrategies.TextReportProcessingStrategy;
+import org.jboss.set.mavendependencyupdater.projectparser.PmeDependencyCollector;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 
-@Mojo(name = "dependency-upgrades-report")
+@Mojo(name = "report")
 public class DependencyReportMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
@@ -23,7 +35,19 @@ public class DependencyReportMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        project.getDependencies().forEach(dependency -> System.out.println(dependency.toString()));
-        project.getDependencyManagement().getDependencies().forEach(dependency -> System.out.println(dependency.toString()));
+        File pomFile = project.getModel().getPomFile();
+        try {
+            Collection<ScopedArtifactRef> rootProjectDependencies = new PmeDependencyCollector(pomFile).getRootProjectDependencies();
+            Configuration configuration = new Configuration(null);
+            TextReportProcessingStrategy strategy = new TextReportProcessingStrategy(configuration, pomFile, System.out);
+            AvailableVersionsResolver availableVersionsResolver = new DefaultAvailableVersionsResolver(configuration);
+            DependencyEvaluator evaluator = new DependencyEvaluator(configuration, availableVersionsResolver);
+            List<DependencyEvaluator.ComponentUpgrade> componentUpgrades = evaluator.getVersionsToUpgrade(rootProjectDependencies);
+            strategy.process(componentUpgrades);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Can't read configuration file.", e);
+        } catch (ManipulationException e) {
+            throw new MojoExecutionException("Problem when collecting project dependencies.", e);
+        }
     }
 }
