5a292fdcf1e1336cfab3562443e73b0282e4828f
Tomas Hofman <thofman@redhat.com>
2020-01-06 16:37:15 +0100

    Maven plugin - perform upgrades task

---
+++ maven-plugin/src/main/java/org/jboss/set/mavendependencyupdater/mavenplugin/PerformUpgradeMojo.java
@@ -0,0 +1,28 @@
+package org.jboss.set.mavendependencyupdater.mavenplugin;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.jboss.set.mavendependencyupdater.DependencyEvaluator;
+import org.jboss.set.mavendependencyupdater.core.processingstrategies.ModifyLocallyProcessingStrategy;
+import org.jboss.set.mavendependencyupdater.core.processingstrategies.TextReportProcessingStrategy;
+import org.jboss.set.mavendependencyupdater.core.processingstrategies.UpgradeProcessingStrategy;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
+
+@Mojo(name = "perform-upgrades")
+public class PerformUpgradeMojo extends AbstractUpdaterMojo {
+
+    @Override
+    protected void processComponentUpgrades(File pomFile, List<DependencyEvaluator.ComponentUpgrade> componentUpgrades)
+            throws MojoExecutionException {
+        getLog().info("Upgrading dependencies in project " + project.getName());
+
+        UpgradeProcessingStrategy strategy = new ModifyLocallyProcessingStrategy(pomFile);
+        strategy.process(componentUpgrades);
+    }
+}
